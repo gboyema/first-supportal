564f5f0e445949a8090e61c064e990768bbc5361
Soyoola Gboyega Akanbi <soyoola.gboyega.akanbi@axis.com>
2021-08-18 12:47:48 +0200

    Account click test

--- src/test/java/axis/webpages/Account.java
+++ src/test/java/axis/webpages/Account.java
@@ -18,8 +18,8 @@ public class Account {
     @FindBy(how = How.XPATH, using = "//a[@title='Ny']")
     WebElement newr;
 
-    @FindAll(@FindBy(how = How.XPATH, using = "//a[@title='Konton']")) //slds-truncate
-    List<WebElement> accounts;
+    @FindBy(how = How.XPATH, using = "//a[@title='Konton']") //slds-truncate
+    WebElement accounts;
 
     public Account (WebDriver driver) {
         this.driver = driver;
@@ -28,14 +28,7 @@ public class Account {
     }
 
     public void clickAccount () {
-        WebDriverWait wait = new WebDriverWait(driver, 15);
-        WebElement elements = wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//a[@title='Konton']")));
-        driver.get("https://axiscommunicationsab2.lightning.force.com/lightning/o/Account/home");
-        System.out.println("text to see");
-        for (WebElement element: accounts) {
-            System.out.println("SOME");
-            System.out.println(element.getText());
-        }
+        driver.get(accounts.getAttribute("href"));
     }
 
     public void clickNew () {
